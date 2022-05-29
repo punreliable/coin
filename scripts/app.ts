3484a563af273830ce444514b0d64c2752962244
Brian <picketfence14@gmail.com>
2022-05-29 15:36:31 -0400

    WiP Bootstrap Modal via TypeScript

---
+++ scripts/app.ts
@@ -0,0 +1,26 @@
+//@ts-check
+
+
+say_hello();
+
+// Coin
+// type Coin
+
+
+function say_hello()
+{
+console.log('Hello World!');
+}
+
+$("#coinModal").modal()
+
+
+/*
+
+when it prints this you've won.
+
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
+
+*/
